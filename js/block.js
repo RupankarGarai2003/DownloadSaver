f742fc505c8b74862615903c41486185eb71cecc
Rupankar Garai <rupankargarai4@gmail.com>
2024-08-30 19:32:23 +0530

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,17 +1,17 @@
-document.addEventListener('keydown', function (e) {
-    // Disable Ctrl+U
-    if (e.ctrlKey && e.key === 'u') {
-        e.preventDefault();
-        console.log('Ctrl+U is disabled.');
-    }
+// document.addEventListener('keydown', function (e) {
+//     // Disable Ctrl+U
+//     if (e.ctrlKey && e.key === 'u') {
+//         e.preventDefault();
+//         console.log('Ctrl+U is disabled.');
+//     }
 
-    // Disable Ctrl+Shift+I, Ctrl+Shift+J, F12
-    if (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J') || e.key === 'F12') {
-        e.preventDefault();
-        console.log('This key combination is disabled.');
-    }
-});
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-    console.log('Right-click is disabled.');
-});
+//     // Disable Ctrl+Shift+I, Ctrl+Shift+J, F12
+//     if (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J') || e.key === 'F12') {
+//         e.preventDefault();
+//         console.log('This key combination is disabled.');
+//     }
+// });
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+//     console.log('Right-click is disabled.');
+// });
